b86c38084d3a6fc144e1883d5b1ffbeb5f99cadf
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-15 10:44:46 +0500

    initial

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
-import {BrowserRouter,Route,Routes} from "react-router-dom"
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Home from './pages/Home'
-import Project from './pages/Project'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import About from './routes/About'
+import Contact from './routes/Contact'
+import Home from './routes/Home'
+import Project from './routes/Project'
+import {Routes,Route} from "react-router-dom"
 
-function App() {
+
+const App = () => {
   return (
-    <BrowserRouter>
-
+    <>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/Contact' element={<Contact/>}/>
-        <Route path='/Project' element={<Project/>}/>
         <Route path='/About' element={<About/>}/>
-        
+        <Route path='/project' element={<Project/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
-    
-    </BrowserRouter>
+
+    </>
   )
 }
 
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,38 +1,34 @@
-import React, { useState } from 'react'
-import { Link } from "react-router-dom"
-import { FaBars, FaTimes } from "react-icons/fa"
+import React from 'react'
+import { Link } from 'react-router-dom'
 import './styles/global.css'
+import {FaBars} from "react-icons/fa"
 
-
-export default function Header() {
-
-    const [nav, setNav] = useState(false);
-
-    return (
-        <>
-            <div className='header w-full h-[80px]  text-[#ffffff] border-2 border-black bg-[#2c3e50] flex_around cursor-pointer'>
-                <p className='text-3xl'>Portfolio</p>
-
-                <div className="links">
-                    <Link to='/' className='text-xl ml-5 mr-1 transition-all hover:text-[#3498db]'>Home</Link>
-                    <Link to='/Project' className='text-xl ml-5 mr-1 transition-all hover:text-[#3498db]'>Projects</Link>
-                    <Link to='/Contact' className='text-xl ml-5 mr-1 transition-all hover:text-[#3498db]'>Contact</Link>
-                    <Link to='/About' className='text-xl ml-5 mr-1 transition-all hover:text-[#3498db]'>About</Link>
-                </div>
-
-                <FaBars onClick={() => setNav(true)} className='lg:hidden md:block sm:block  text-3xl font-black' />
-
+function Header() {
+  return (
+    <div className='Header flex justify-between items-center'>
+            <Link to="/">
+                <h1>Portfolio</h1>
+            </Link>
+            <ul className='nav-menu'>   
+                <li>
+                    <Link to="/" className='text-[#fff]'>Home</Link>
+                </li>
+                <li>
+                    <Link to="/project" className='text-[#fff]'>Project</Link>
+                </li>
+                <li>
+                    <Link to="/About" className='text-[#fff]'>About</Link>
+                </li>
+                <li>
+                    <Link to="/contact" className='text-[#fff]'>Contact</Link>
+                </li>
+                
+            </ul>
+            <div className="hamburger">
+                <FaBars size={20} style={{color:"#fff",cursor:"pointer"}}/>
             </div>
-
-            <div className={`mobile flex_around flex-col relative ${nav === true ? '' : 'hidden'} `}>
-                <FaTimes onClick={() => setNav(false)} className='text-4xl absolute left-0 top-0 text-white cursor-pointer' />
-
-                <Link to='/' className='text-3xl  text-white ml-5 mr-1 transition-all hover:text-[#3498db]'>Home</Link>
-                <Link to='/Project' className='text-3xl  text-white ml-5 mr-1 transition-all hover:text-[#3498db]'>Projects</Link>
-                <Link to='/Contact' className='text-3xl  text-white ml-5 mr-1 transition-all hover:text-[#3498db]'>Contact</Link>
-                <Link to='/About' className='text-3xl  text-white ml-5 mr-1 transition-all hover:text-[#3498db]'>About</Link>
-
-            </div>
-        </>
-    )
+    </div>
+  )
 }
+
+export default Header
