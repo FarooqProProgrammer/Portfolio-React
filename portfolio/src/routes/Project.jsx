e7a838c86aaf807bb67765fb8b1acf53ccd8be6d
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-15 20:28:52 +0500

    n

--- portfolio/src/routes/Project.jsx
+++ portfolio/src/routes/Project.jsx
@@ -3,12 +3,14 @@ import Cards from '../components/Cards'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Her2 from '../components/Her2'
+import WorkCard from '../components/WorkCard'
 
 function Project() {
   return (
     <div>
       <Header/>
       <Her2  heading="PROJECTS." paragraph="Some of my Most Recent Works " />
+      <WorkCard/>
       <Cards/>
       <Footer/>
     </div>
