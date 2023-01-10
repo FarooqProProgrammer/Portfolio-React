ad35626a4d65a569477bd2efcdd961d3aad87467
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-01-10 22:07:52 +0500

    Updated

---
+++ hospital/src/common/Gallery/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div className='w-full h-[600px] border-2 border-black grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-40 place-items-center'>
+            <div className="imageCompnent w-[200px] h-[200px] border-2 border-black">
+
+            </div>
+            <div className="imageCompnent w-[200px] h-[200px] border-2 border-black">
+
+            </div>
+            <div className="imageCompnent w-[200px] h-[200px] border-2 border-black">
+
+            </div>
+            <div className="imageCompnent w-[200px] h-[200px] border-2 border-black">
+
+            </div>
+            <div className="imageCompnent w-[200px] h-[200px] border-2 border-black">
+
+            </div>
+            
+    </div>
+  )
+}
+
+export default Gallery
--- hospital/src/components/pages/Home.jsx
+++ hospital/src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../../common/Hero/Hero'
 import Service from '../../common/Service'
+import Gallery from '../../common/Gallery'
 
 const Home = () => {
   return (
@@ -12,6 +13,10 @@ const Home = () => {
                 <button className='text-3xl font-black border-2 hover:border-4 hover:text-gray-400 border-white text-white pl-3 pb-3 pt-3 pr-3 cursor-pointer '>Add New Services</button>
         </div>
         <Service/>
+        <div className="textContainer w-full h-[70px]  bg-[#3498db] flex justify-around items-center">
+               <p className='text-5xl font-black text-white hover:text-gray-200'>Galleries</p>
+        </div>
+        <Gallery/>
     </div>
   )
 }
